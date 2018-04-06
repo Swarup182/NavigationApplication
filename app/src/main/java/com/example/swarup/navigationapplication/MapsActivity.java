de26f19a166fbdc3783871259358ca3aa3a800ce
Swarup <swarup.rajbhandari@gmail.com>
2018-04-06 13:35:40 +1000

    Update MapsActivity.java

--- app/src/main/java/com/example/swarup/navigationapplication/MapsActivity.java
+++ app/src/main/java/com/example/swarup/navigationapplication/MapsActivity.java
@@ -13,12 +13,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double selectedLatitude;
+    private double selectedLongitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        selectedLatitude = getIntent().getExtras().getDouble("latitude");
+        selectedLongitude = getIntent().getExtras().getDouble("longitude");
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -39,7 +46,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(selectedLatitude, selectedLongitude);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
